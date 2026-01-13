6d74df79fdb739e0ca3503df35f7524d5665083e
cenat47 <000099909909a@gmail.com>
2026-01-14 00:35:51 +0300

    add Alembic migration setup and initial database model for ticker prices

---
+++ src/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Float, String, BigInteger
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.database import Base  
+
+
+class TickerPriceORM(Base):
+    __tablename__ = "ticker_prices"
+
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True, autoincrement=True)
+    ticker: Mapped[str] = mapped_column(String(10), nullable=False)  
+    price: Mapped[float] = mapped_column(Float, nullable=False)       
+    timestamp: Mapped[int] = mapped_column(BigInteger, nullable=False)  
+
